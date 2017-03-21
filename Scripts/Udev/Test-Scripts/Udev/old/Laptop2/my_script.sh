8d2b63bbf62757d14314b45147842e73b4f4ac8e
SheaONeill <sheaoneill@hotmail.com>
2017-03-21 02:12:28 +0000

    Ignoring old test files

--- Scripts/Udev/Test-Scripts/Udev/old/Laptop2/my_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#shutdown -r now
-sleep 2
-/bin/echo pwd
-/usr/local/bin/my_script2.sh & 
-
-
--- Scripts/Udev/Test-Scripts/Udev/old/Laptop2/my_script2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/gnome-terminal &
-#/usr/bin/gnome-terminal -x bash -c "tail -f /var/log/syslog" 
-#/usr/bin/gnome-terminal -e 'bash -c "tail -f /var/log/syslog"' & 
