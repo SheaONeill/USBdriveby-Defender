69f65c8554490d20679c4eb88a8effb8283d0799
SheaONeill <sheaoneill@hotmail.com>
2017-03-21 06:33:59 +0000

    Modified Scripts

--- Scripts/Udev/bin/my_script2.sh
+++ Scripts/Udev/bin/my_script2.sh
@@ -10,10 +10,13 @@ export XAUTHORITY=/root/.Xauthority
 #to do get specific details using ne line of code for vendor etc
 #
 #test to get specific product details and output to file udevadm_info
-/sbin/udevadm info -a -p $(udevadm info -q path -n /dev/sdb1) | grep 'idVendor\|idProduct' | head -2 > ~/Desktop/udevadm_info
-# result:  ATTRS{idProduct}=="800e"
+/sbin/udevadm info -a -p $(udevadm info -q path -n /dev/sdb1) | grep 'idVendor\|idProduct' | head -2 | cut -d"\"" -f2 > ~/Desktop/udevadm_info
+# 
 #
+#product=$(cat /root/Desktop/udevadm_info | tail -1)
+#vendor=$(cat /root/Desktop/udevadm_info | tail -2)
 #
+#echo "Vendor $vendor Product $product"
 # this gives a notification that device is inserted
 su root -c 'zenity --notification --text="Device Inserted!"'
 #
--- Scripts/Udev/bin/post_remove.sh
+++ Scripts/Udev/bin/post_remove.sh
@@ -2,4 +2,9 @@
 cd /root/Desktop
 echo "Device Removed"
 echo "last 5 modified files:"
+product=$(cat /root/Desktop/udevadm_info | tail -1)
+vendor=$(cat /root/Desktop/udevadm_info | head -1)
+#
+echo "Vendor $vendor Product $product"
+
 ls -Art | tail -n5
