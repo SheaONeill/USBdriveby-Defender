51ab560dbf7249b1b8c5120de4e3908b1f69276d
SheaONeill <sheaoneill@hotmail.com>
2017-03-22 20:15:13 +0000

    Added Install and DB setup scripts

---
+++ Scripts/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#copy rules to udev directory
+rules_setup () {
+    cp {63-usb-kbd.rules,81-usb-flash.rules} /etc/udev/rules.d/
+    ls -la /etc/udev/rules.d/
+    #reload rules: 
+    udevadm control --reload-rules
+}
+
+#copy scripts to /usr/local/bin/
+copy_scripts () {
+    cp {keyboard,keyboard_udev,dialog-menu} /usr/local/bin/
+    chmod +x /usr/local/bin/*
+    ls -la /usr/local/bin/
+}
+
+#setup database function
+setup_database () {
+    #setup database call dbsetup script and pass values
+    ./dbsetup $host $dbname $name $pass
+}
+
+#get user input
+get_db_details () {
+    echo -n "database host: "
+    read host
+    #if $host="q" then exit 
+    echo -en "database name: "
+    read dbname
+    echo -en "username: "
+    read name
+    echo -en "password: "
+    read pass    
+}
+ main () {
+     #call rules_setup function
+     rules_setup
+     #call copy_scripts function
+     copy_scripts
+     #call get_db_details function
+     get_db_details
+     #call setup_database function
+     setup_database   
+}
+
+main "$@"
+
