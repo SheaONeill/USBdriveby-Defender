441b54e9573d0bcb6234cc7113893c3f2cec7a9b
SheaONeill <sheaoneill@hotmail.com>
2017-04-05 15:14:03 +0100

    LAtest Update!

--- Scripts/Setup/copy_and_commit.sh
+++ Scripts/Setup/copy_and_commit.sh
@@ -8,9 +8,10 @@
 #copy rules to git repo
 LOCAL_BIN_PATH="/usr/local/bin/"
 LOCAL_RULES_PATH="/etc/udev/rules.d/"
-GIT_REPO_PATH="/media/root/DATA/Dropbox/Year_3/Semester_2/Project/Github_Local-Repo/Scripts/"
-GIT_REPO_RULES_PATH="/Udev/Rules/ "
-GIT_REPO_BIN_PATH="/Udev/bin/ "
+GIT_REPO_PATH="/media/root/DATA/Dropbox/Year_3/Semester_2/Project/Github_Local-Repo/"
+GIT_REPO_RULES_PATH="Scripts/Udev/Rules/ "
+GIT_REPO_BIN_PATH="Scripts/Udev/bin/ "
+GIT_REPO_SETUP_PATH="Scripts/"
 BASHRC_PATH="$HOME/.bashrc"
 #test
 echo -e "\nPaths: Local :${LOCAL_RULES_PATH} \nGit  ${GIT_REPO_PATH} \nGit Rules: ${GIT_REPO_RULES_PATH}"
@@ -18,25 +19,43 @@ echo "Copying ${LOCAL_RULES_PATH}*.rules to ${GIT_REPO_PATH}${GIT_REPO_RULES_PAT
 echo "press ENTER to continue"; read a
 #copy all rules from local to git
 cp "${LOCAL_RULES_PATH}"*.rules ${GIT_REPO_PATH}${GIT_REPO_RULES_PATH}
+echo
 ls ${GIT_REPO_PATH}${GIT_REPO_RULES_PATH}
 #
 #test
-echo "Copying ${LOCAL_BIN_PATH}*.sh to ${GIT_REPO_PATH}${GIT_REPO_BIN_PATH}"
+echo -e "\nCopying ${LOCAL_BIN_PATH}*.sh to ${GIT_REPO_PATH}${GIT_REPO_BIN_PATH}"
 echo "press ENTER to continue"; read a
 #copy all scripts from local to git
 cp "${LOCAL_BIN_PATH}"*.sh ${GIT_REPO_PATH}${GIT_REPO_BIN_PATH}
+echo
 ls ${GIT_REPO_PATH}${GIT_REPO_BIN_PATH}
 #
 #test
-echo "Copying ${LOCAL_BIN_PATH}Setup/  to ${GIT_REPO_PATH}"
+echo -e "\nCopying ${LOCAL_BIN_PATH}Setup/  to ${GIT_REPO_PATH}${GIT_REPO_SETUP_PATH}"
 echo "press ENTER to continue"; read a
 #copy setup scripts from local to git
-cp  -r "${LOCAL_BIN_PATH}Setup/" ${GIT_REPO_PATH}
+cp  -r "${LOCAL_BIN_PATH}Setup/" ${GIT_REPO_PATH}${GIT_REPO_SETUP_PATH}
+echo
 ls ${GIT_REPO_PATH}Setup/
 #
 #test
-echo "Copying ${BASHRC_PATH}  to ${GIT_REPO_PATH}"
+echo -e "\nCopying ${BASHRC_PATH}  to ${GIT_REPO_PATH}"
 echo "press ENTER to continue"; read a
 #copy scripts from local to git
 cp "${BASHRC_PATH}" ${GIT_REPO_PATH}
+echo
 ls -a ${GIT_REPO_PATH}
+#
+#git pull origin master
+echo  -e "\nChanging Directory"
+cd ${GIT_REPO_PATH}
+echo -e "\nChecking status...."; git status
+echo -e "\nPulling remote files now!"; git pull origin master
+echo -e "\nCommiting files locally now!"; git add -A
+echo -e "\nChecking status...."; git status
+echo -e "\nEnter Commit Message now!"; read commit_message
+echo -e "\nCommit Message : ${commit_message}"; git commit -m "${commit_message}"
+echo -e "\nChecking status...."; git status
+echo -e "\nPushing files to remote now!"; git push origin master
+
+
