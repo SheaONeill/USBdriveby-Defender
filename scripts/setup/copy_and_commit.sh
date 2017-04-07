72ac9da9d8a26fa6ab0ad2ebc58077ccf2bbcb5c
SheaONeill <sheaoneill@hotmail.com>
2017-04-07 21:20:28 +0100

    update

--- scripts/setup/copy_and_commit.sh
+++ scripts/setup/copy_and_commit.sh
@@ -15,7 +15,8 @@ GIT_REPO_SETUP_PATH="scripts/"
 BASHRC_PATH="$HOME/.bashrc"
 #test
 echo -e "\nPaths: Local :${LOCAL_RULES_PATH} \nGit  ${GIT_REPO_PATH} \nGit Rules: ${GIT_REPO_RULES_PATH}"
-echo "Copying ${LOCAL_RULES_PATH}*.rules to ${GIT_REPO_PATH}${GIT_REPO_RULES_PATH}"
+echo "Copying ${LOCAL_RULES_PATH}63-usb-kbd.rules ${GIT_REPO_PATH}${GIT_REPO_RULES_PATH}"
+echo "Copying ${LOCAL_RULES_PATH}81-usb-flash.rules ${GIT_REPO_PATH}${GIT_REPO_RULES_PATH}"
 echo "press ENTER to continue"; read a
 #copy all rules from local to git
 cp "${LOCAL_RULES_PATH}"63-usb-kbd.rules ${GIT_REPO_PATH}${GIT_REPO_RULES_PATH}
