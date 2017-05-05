9b560f7ba56460b20f58377c5fd10752a0ecfe46
SheaONeill <sheaoneill@hotmail.com>
2017-05-05 02:51:39 +0100

    Update get keystroke speeds

--- scripts/udev/bin/get_char.sh
+++ scripts/udev/bin/get_char.sh
@@ -21,14 +21,17 @@ read_char () {
 	#call check_time
 	#get_time 
 	#call check_enter_key function
-    check_enter_key 
+    check_character_key 
 	stty icanon echo
     }
 
-#this function checks and sets if newline
-check_enter_key () {
+#this function checks and sets if newline or space character
+check_character_key () {
     if [ "$input_char" = $'\n' ]; 
         then newline=true
+    fi
+    if [ "$input_char" = " " ]; 
+    then space=true
     fi
 	}
 
@@ -40,62 +43,255 @@ stty icanon echo
 exit 2
 }	
 
-#get_time
-get_time() {
-#set current time to var array strip floating point
-#ks[$count]=$(echo "$(date +%s.%N)" | bc -l)
-ks[$count]=$(bc <<< "$(date +%N)")
-echo -e "\nNanoseconds Now: ${ks[$count]}"
-echo -e "\nCount Now: ${count}"
-#check if second keypress then calculate
-if [ $count -gt 1 ]; then
-    #speed[$(($count-1))]=$(echo "$((ks[$((count))]))" - "$((ks[$((count-1))]))" | bc)
-    #subtract second keystrokefrom first and set to result array
-    #speed[$(($count-1))]=$(bc <<< "${ks[$count]}-${ks[$count-1]}")
-    echo -e "\nTest 01: $((${ks[$count-1]}-${ks[$count]}))"
-    testdiff=$((${ks[$count-1]}-${ks[$count]}))
-    echo -e "\n testdiff: $testdiff"
-    break
-    
-    speed[$(($count-1))]=$((${ks[$count-1]}-${ks[$count]}))
-    echo -e "\nSpeed:  speed[$(($count-1))]"
-    #check if more than three keypresses then calculate (these values can be altered obviously
-    if [ $count -gt 2 ] && [ $count -lt 5 ]; then 
-        #add speed differences 
-        #
-        #getting standard input error here why???????????????????????
-        #
-   echo -e "\n\n*******************************************"
-        #don't need all the echo shit remove when all good
-   echo -e "\nSpeed: ${speed[$(($count-1))]}"
-        #
-        #total=$(bc <<< "${speed[$count-1]}+${speed[$count]}")
-        echo -e "\nTest 02: $((${speed[$count-1]}+${speed[$count]}))"
-        total="$((${speed[$count-1]}+${speed[$count]}))"
-        echo -e "\nTotal : $total"
-        
-        if [ $count -eq 4 ]; then
-            
-            echo -e "\ncount @=$count"
-            #avg=$(bc <<<"${total} / ${count}")
-            avg=$(( ${total} / ${count} ))
-            #export average so main shell can access/see it bitch
-            export avg
-            #test avg
-            echo -e "Average speed = $avg"
+check_timings(){
+    #ref: http://stackoverflow.com/questions/385408/get-program-execution-time-in-the-shell
+
+    #subtract start time from now time and set array value
+    keystroke[${count}]=$(echo "$(date +%N) - $start" | bc -l)
+    #printf "Execution time: %.6f seconds" ${keystroke[${count}]}
+    echo -e "\nKeystroke Array Var ${count}: ${keystroke[${count}]}"
+    echo -e "\nnewline $newline"
+    #check if character count is at eight words
+    if [  ${count} -gt 8 ] && [ "${newline}" == "true" ]
+        then 
+            echo -e "\nnewline $newline"
+            echo -e "\nTarget reached ${count}"  
+            #echo -en "\nAverage time taken: " $(( total_taken / count ))
+            echo -e "\nAverage difference = $avg_diff"
+            echo $avg_diff >> ${LOG_PATH}difference_averages.log
+            echo -e "\nDifference_averages.logs saved!"
+            echo $(( total_taken / count )) >> ${LOG_PATH}keystroke_averages.log
+            echo -e "\nKeystroke_averages.logs saved!"
+            #get total from file
+            #SUM=0; for i in `cat keystroke_averages.log`; do SUM=$(($SUM + $i)); done; echo $SUM
+            #wordcount=$(( wc keystroke_averages.log))
+            #AVG=$(( SUM / wordcount))
+            #ref :http://stackoverflow.com/questions/15224581/floating-point-comparison-with-variable-in-bash
+	        #if (( $(echo "$(( total_taken / count ))  < ${lim}" | bc -l) )); then
+	        echo -e "\nIs this the error"
+	        if  [ "${avg_diff}"  -lt  "${diff_limit}" ]; then
+	            #log averages 
+                echo $(( total_taken / count )) >> ${LOG_PATH}keystroke_averages_alert.log
+                echo -e "\nKeystroke_averages_alert.log saved!"
+                echo $avg_diff >> ${LOG_PATH}difference_averages._alert.log
+	            echo -e "\nDifference_averages_alert.log saved!"
+	            
+	            echo -e "speed ${avg_diff} is less than ${diff_limit}"
+            	echo -e "\nNon Human Detected!"
+		        
+		        #double-check that flag is set
+		        if [ -f ${LOG_PATH}keyboard_flag ]; then
+			        echo "resetting keyboard_flag to 1"
+			        #set flag to 1 (non-human)
+			        echo 1 > ${LOG_PATH}keyboard_flag
+		        else
+		        echo -e "\nno keyboard_flag file found"	
+	            fi
+	
+	            #call unbind device
+	            echo -e "\nunbind device"
+                #/usr/local/bin/unbind_device.sh
+                #
+                #call take_photo
+                echo -e "\ntake_photo"
+                #/usr/local/bin/take_photo.sh
+                #
+                #call contact_admin
+                echo -e "\ncontact_admin"
+	             /usr/local/bin/contact_admin.sh
+	            #                
+                #call lock_down
+                echo -e "\nlock down"
+                # /usr/local/bin/lock_down.sh
+                #
+                #call sound_alarm
+                echo -e "\nsound_alarm"
+                #/usr/local/bin/sound_alarm.sh
+                #
+                #addons
+	            #upload captured details to server in case system gets compromised
+	            #run a check on all opened ports and shut them down where applicable
+	            #check captured commands and compare against known scripts
+	            
+	            #
+	        
+        else echo -e "speed ${avg_diff} is greater than ${diff_limit}";
+            #set flag to 0 (human)
+                echo -e "\nHuman Detected!"
+                echo -e "\nnewline $newline"
+                #run command from log file
+                bash ${LOG_PATH}keyboard_input.log
+                echo 0 > ${LOG_PATH}keyboard_flag
+                echo "" > ${LOG_PATH}keyboard_input.log
+                exit
+              
         fi
-        echo -e "*******************************************\n\n"
     fi
-    #don't need all the echo shit remove when all good
-   # echo -e "\nSpeed: ${speed[$(($count-1))]}"
-fi
-echo -e "\n\n========================"
-echo -e "\nCount=$count"
-echo -e "\nKeystroke_01=: ${ks[$count]}"
-echo -e "\nKeystroke_02=: ${ks[$count-1]}"
-count=$(($count+1));
-#echo -e "\nCount Now: $count"
-echo -e "========================\n\n"
+    
+   if [ ${count} -le 8 ];then 
+       #add speeds
+        total_taken=$(( total_taken + keystroke[${count}] ))
+       #
+        echo -e "\nAverages: Total: $total_taken Divided by Count: $count = $(( total_taken / count ))" 
+       #
+        if [ ${count} -gt 1 ];then 
+             #$((${ks[$count-1]}-${ks[$count]}))
+             echo -e "\nKeystroke Now: ${keystroke[$count]} Keystroke Before: ${keystroke[$count-1]} " 
+           #I am an INT
+            #declare -i difference
+            #declare -i speed[$(($count-1))]
+            #difference=$(echo "${keystroke[$count]} - ${keystroke[$count-1]}" | bc )
+            speed[$(($count-1))]=$(echo "${keystroke[$count]} - ${keystroke[$count-1]}" | bc )
+             echo -en "\nSpeed between :  ${keystroke[$count]} and ${keystroke[$count-1]}: "
+            #
+            
+           #keystroke[${count}]=$(echo "$(date +%N) - $start" | bc)
+           #display difference
+           # echo -e "\n Difference: "$(($difference | bc ))  
+             echo -e "\n Speed: "$((speed[$(($count-1))] | bc ))  
+            #check if negative
+               #if [[ $difference  -lt 0 ]]
+               if [[ ${speed[$(($count-1))]}  -lt 0 ]]
+                then
+                   # echo "$difference  Not positive"
+                   echo "${speed[$(($count-1))]}  Speed Not positive"
+                    #remove negative char
+                   # difference=${difference/#-/}
+                    speed[$(($count-1))]=${speed[$(($count-1))]/#-/}
+                    echo "[+] ${speed[$(($count-1))]}  Is now!"
+                else
+                    echo "[X] ${speed[$(($count-1))]}  Passes check"
+                fi  
+             #add speeds
+           # total_diff=$(( total_diff + difference))
+            total_diff=$(( total_diff + speed[$(($count-1))]))
+            #avg_diff=$(( ${total_diff} / ${count} ))
+            avg_diff=$(( ${total_diff} / ${count} ))
+            #export average so main shell can access/see it bitch
+            export avg_diff
+            #test avg_diff
+            echo -e "\nTotal Speed= $total_diff"
+            echo -e "\nAverage Speed= $avg_diff"
+            
+        fi
+    fi
+    
+    #increment counter
+    count=$(($count+1));
+   
+}
+
+check_timings2(){
+    #ref: http://stackoverflow.com/questions/385408/get-program-execution-time-in-the-shell
+
+    #subtract start time from now time and set array value
+    dur[${count}]=$(echo "$(date +%N) - $start" | bc -l)
+    #printf "Execution time: %.6f seconds" ${dur[${count}]}
+    echo -e "\nDifference Array Var ${count}: ${dur[${count}]}"
+   
+    #check if character count is at eight words
+    if [  ${count} -eq 8 ]
+        then 
+            echo -e "\nEight reached ${count}"  
+            echo -en "\nAverage time taken: " $(( total_taken / count ))
+            echo -e "\nAverage difference = $avg_diff"
+            echo $avg_diff >> ${LOG_PATH}difference_averages.log
+             echo -e "\nDifference_averages.logs saved!"
+             echo $(( total_taken / count )) >> ${LOG_PATH}keystroke_averages.log
+             echo -e "\nKeystroke_averages.logs saved!"
+            #get total from file
+            #SUM=0; for i in `cat keystroke_averages.log`; do SUM=$(($SUM + $i)); done; echo $SUM
+            #wordcount=$(( wc keystroke_averages.log))
+            #AVG=$(( SUM / wordcount))
+            #ref :http://stackoverflow.com/questions/15224581/floating-point-comparison-with-variable-in-bash
+	        if (( $(echo "$(( total_taken / count ))  < ${lim}" | bc -l) )); then
+	            #log averages 
+                echo $(( total_taken / count )) >> ${LOG_PATH}keystroke_averages_alert.log
+                echo -e "\nKeystroke_averages_alert.log saved!"
+                echo $avg_diff >> ${LOG_PATH}difference_averages._alert.log
+	            echo -e "\nDifference_averages_alert.log saved!"
+	            
+	            echo -e "speed ${dur[${count}]} is less than ${lim}"
+            	echo -e "\nNon Human Detected!"
+		        
+		        #double-check that flag is set
+		        if [ -f ${LOG_PATH}keyboard_flag ]; then
+			        echo "resetting keyboard_flag to 1"
+			        #set flag to 1 (non-human)
+			        echo 1 > ${LOG_PATH}keyboard_flag
+		        else
+		        echo -e "\nno keyboard_flag file found"	
+	            fi
+	
+	            #call unbind device
+                #/usr/local/bin/unbind_device.sh
+                #
+                #call take_photo
+               # /usr/local/bin/take_photo.sh
+                #
+                #contact_admin
+	            # /usr/local/bin/contact_admin.sh
+	            #                
+                #lock_down
+                # /usr/local/bin/lock_down.sh
+                #
+                #call sound_alarm
+                #/usr/local/bin/sound_alarm.sh
+                #
+                #addons
+	            #upload captured details to server in case system gets compromised
+	            #run a check on all opened ports and shut them down where applicable
+	            #check captured commands and compare against known scripts
+	            
+	            #
+	        
+        else echo -e "speed ${dur[${count}]} is greater than ${lim}";
+            #set flag to 0 (human)
+                echo -e "\nHuman Detected!"
+                echo 0 > ${LOG_PATH}keyboard_flag
+              
+        fi
+    fi
+    
+   if [ ${count} -le 8 ];then 
+       #add speeds
+        total_taken=$(( total_taken + dur[${count}] ))
+       #
+        echo -e "\nAverages: Total:$total_taken Divided by Count: $count = $(( total_taken / count ))" 
+       #
+        if [ ${count} -gt 1 ];then 
+             #$((${ks[$count-1]}-${ks[$count]}))
+             echo -e "\nDuration Now: ${dur[$count]} Duration Before: ${dur[$count-1]} " 
+           #I am an INT
+            declare -i testdiff
+            testdiff=$(echo "${dur[$count]} - ${dur[$count-1]}" | bc )
+           #dur[${count}]=$(echo "$(date +%N) - $start" | bc)
+           #display difference
+            echo -e "\n Difference: "$(($testdiff | bc ))  
+            #check if negative
+               if [[ $testdiff  -lt 0 ]]
+                then
+                    echo "$testdiff  Not positive"
+                    #remove negative char
+                    testdiff=${testdiff/#-/}
+                    echo "[+] $testdiff  Is now!"
+                else
+                    echo "[X] $testdiff  Passes check"
+                fi  
+             #add speeds
+            total_diff=$(( total_diff + testdiff))
+            avg_diff=$(( ${total_diff} / ${count} ))
+            #export average so main shell can access/see it bitch
+            export avg_diff
+            #test avg_diff
+            echo -e "\nAverage Difference = $avg_diff"
+            
+        fi
+    fi
+    
+    #increment counter
+    count=$(($count+1));
+   
 }
 
 
--- scripts/udev/bin/get_input.sh
+++ scripts/udev/bin/get_input.sh
@@ -14,119 +14,10 @@ echo -e "\nPath Log Name ${PATH_LOG_NAME} \nIn Get Standard Input " >> ${LOG_PAT
 export count=1
 #fix this limit when speed is sorted
 lim=2363480
-diff_limit=250000
-check_timings(){
-    #ref: http://stackoverflow.com/questions/385408/get-program-execution-time-in-the-shell
+diff_limit=300000
+newline="false"
+space="false"
 
-    #subtract start time from now time and set array value
-    dur[${count}]=$(echo "$(date +%N) - $start" | bc -l)
-    #printf "Execution time: %.6f seconds" ${dur[${count}]}
-    echo -e "\nDifference Array Var ${count}: ${dur[${count}]}"
-   
-    #check if character count is at eight words
-    if [  ${count} -eq 8 ]
-        then 
-            echo -e "\nEight reached ${count}"  
-            echo -en "\nAverage time taken: " $(( total_taken / count ))
-            echo -e "\nAverage difference = $avg_diff"
-            echo $avg_diff >> ${LOG_PATH}difference_averages.log
-             echo -e "\nDifference_averages.logs saved!"
-             echo $(( total_taken / count )) >> ${LOG_PATH}keystroke_averages.log
-             echo -e "\nKeystroke_averages.logs saved!"
-            #get total from file
-            #SUM=0; for i in `cat keystroke_averages.log`; do SUM=$(($SUM + $i)); done; echo $SUM
-            #wordcount=$(( wc keystroke_averages.log))
-            #AVG=$(( SUM / wordcount))
-            #ref :http://stackoverflow.com/questions/15224581/floating-point-comparison-with-variable-in-bash
-	        if (( $(echo "$(( total_taken / count ))  < ${lim}" | bc -l) )); then
-	            #log averages 
-                echo $(( total_taken / count )) >> ${LOG_PATH}keystroke_averages_alert.log
-                echo -e "\nKeystroke_averages_alert.log saved!"
-                echo $avg_diff >> ${LOG_PATH}difference_averages._alert.log
-	            echo -e "\nDifference_averages_alert.log saved!"
-	            
-	            echo -e "speed ${dur[${count}]} is less than ${lim}"
-            	echo -e "\nNon Human Detected!"
-		        
-		        #double-check that flag is set
-		        if [ -f ${LOG_PATH}keyboard_flag ]; then
-			        echo "resetting keyboard_flag to 1"
-			        #set flag to 1 (non-human)
-			        echo 1 > ${LOG_PATH}keyboard_flag
-		        else
-		        echo -e "\nno keyboard_flag file found"	
-	            fi
-	
-	            #call unbind device
-                /usr/local/bin/unbind_device.sh
-                #
-                #call take_photo
-                /usr/local/bin/take_photo.sh
-                #
-                #contact_admin
-	             /usr/local/bin/contact_admin.sh
-	            #                
-                #lock_down
-                 /usr/local/bin/lock_down.sh
-                #
-                #call sound_alarm
-                /usr/local/bin/sound_alarm.sh
-                #
-                #addons
-	            #upload captured details to server in case system gets compromised
-	            #run a check on all opened ports and shut them down where applicable
-	            #check captured commands and compare against known scripts
-	            
-	            #
-	        
-        else echo -e "speed ${dur[${count}]} is greater than ${lim}";
-            #set flag to 0 (human)
-                echo -e "\nHuman Detected!"
-                echo 0 > ${LOG_PATH}keyboard_flag
-              
-        fi
-    fi
-    
-   if [ ${count} -le 8 ];then 
-       #add speeds
-        total_taken=$(( total_taken + dur[${count}] ))
-       #
-        echo -e "\nAverages: Total:$total_taken Divided by Count: $count = $(( total_taken / count ))" 
-       #
-        if [ ${count} -gt 1 ];then 
-             #$((${ks[$count-1]}-${ks[$count]}))
-             echo -e "\nDuration Now: ${dur[$count]} Duration Before: ${dur[$count-1]} " 
-           #I am an INT
-            declare -i testdiff
-            testdiff=$(echo "${dur[$count]} - ${dur[$count-1]}" | bc )
-           #dur[${count}]=$(echo "$(date +%N) - $start" | bc)
-           #display difference
-            echo -e "\n Difference: "$(($testdiff | bc ))  
-            #check if negative
-               if [[ $testdiff  -lt 0 ]]
-                then
-                    echo "$testdiff  Not positive"
-                    #remove negative char
-                    testdiff=${testdiff/#-/}
-                    echo "[+] $testdiff  Is now!"
-                else
-                    echo "[X] $testdiff  Passes check"
-                fi  
-             #add speeds
-            total_diff=$(( total_diff + testdiff))
-            avg_diff=$(( ${total_diff} / ${count} ))
-            #export average so main shell can access/see it bitch
-            export avg_diff
-            #test avg_diff
-            echo -e "\nAverage Difference = $avg_diff"
-            
-        fi
-    fi
-    
-    #increment counter
-    count=$(($count+1));
-   
-}
 
 #call function from external script
 source /usr/local/bin/get_char.sh
@@ -142,15 +33,23 @@ while read_char input_char; do
     echo -e "\nStart DateTime Now:  $start\n"
     #call check_timings
     check_timings
+    #get_time
     
-    #this checks for a newline
-    check_enter_key
+    #this checks if newline or space character was entered
+    check_character_key
     if [ "$newline" = "true" ]; 
-        then input_char="\nENTER"
+        then 
         #append newline and value
-        echo -e $input_char >> ${LOG_PATH}keyboard_input.log
+        echo -e "\n" >> ${LOG_PATH}keyboard_input.log
+        #echo '\n' >> ${LOG_PATH}keyboard_input.log
         #reset bool
         newline="false"
+     elif [ "$space" = "true" ]; 
+        then 
+        #append newline and value
+        echo -n " " >> ${LOG_PATH}keyboard_input.log
+        #reset bool
+        space="false"
     else
         #append $input_char no line breaks
         echo -n $input_char >> ${LOG_PATH}keyboard_input.log
